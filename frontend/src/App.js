fcb7ef93d5b53bc7d2fc7234fcc866c82da7c287
Bryant Wiggins <topwigit@gmail.com>
2023-07-18 20:10:57 -0400

    made home route path

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import Home from './Home'
+import Navbar from './Header/Navbar';
 import React from 'react';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import './index.css';
@@ -10,9 +11,19 @@ function App() {
 
   return (
     <div className='main'>
+      <nav className='navbar-container'>
+        <Navbar />
+      </nav>
+
       <div>
-        
+        <Routes>
+          <Route path ='/' element= {<Home />}>
+            
+          </Route>
+        </Routes>
       </div>
+
+
       
     </div>
 
