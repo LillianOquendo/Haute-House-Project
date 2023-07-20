a20eb24234f87b3faca9b7207d4eef359a39b0d0
Lillian Oquendo <LillianOquendo1@gmail.com>
2023-07-19 21:39:25 -0400

    updated contact us page

--- frontend/src/Contact.js
+++ frontend/src/Contact.js
@@ -1,13 +1,21 @@
 import React from "react";
 
-function Contact() {
 
+function Contact() {
+    const phoneNumber = '123-456-7891';
+    const address = '123 1st Ave, New York, New York 10009';
+    const email = 'HauteHouseSalon@randomsite.com';
 
     return (
         <div>
-
+            <h2 className="text-2xl font-bold mb-4">Contact Us</h2>
+            <p>Phone: {phoneNumber}</p>
+            <p>Address: {address}</p>
+            <p>Email: {email}</p>
+            
         </div>
-
+/*We can use leaflet css to add a map feature to this page. 
+Additionally, we can incorporate fake social media buttons*/
     )
 
 }
