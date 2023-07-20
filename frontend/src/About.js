4185afb36702883278c077680f5e501f6c149e7c
Lillian Oquendo <LillianOquendo1@gmail.com>
2023-07-20 10:21:55 -0400

    'updated about and contact pages'

--- frontend/src/About.js
+++ frontend/src/About.js
@@ -1,11 +1,31 @@
 import React from "react";
 
+//import Navbar from "./Header/Navbar";
+
 
 function About() {
-
+const about_photo = "https://i.ibb.co/1qXqG2T/Screenshot-2023-07-18-223132.jpg"
 
     return (
-        <div>
+        <div className="About-Container">
+            <br></br>
+            <br></br>
+            <h1 className="text-7xl text-center">About Us</h1>
+            <br></br>
+            <p className="text-center">Welcome to Haute House, where hair dreams come to life!<br></br>
+            Our exclusive hair salon is a haven of style, sophistication, and luxury, serving the most discerning clientele in the industry. <br></br>
+            With a passion for hair that knows no bounds, our trio of owners and lead stylists Lillian, Tony, and Bryant have been transforming <br></br>
+            tresses into works of art for over three years.
+            <br></br>
+            <br></br>
+            With each visit to Haute House, our clients embark on a journey of self-expression, guided by our talented team of stylists. <br></br>
+            Whether it's a glamorous Hollywood makeover or a daring transformation, we pride ourselves on delivering exceptional service <br></br>
+            that surpasses all expectations.
+            </p>
+            <div className="h-screen flex items-center justify-center">
+            <img src="https://i.ibb.co/1qXqG2T/Screenshot-2023-07-18-223132.jpg" alt="About Photo"/>
+            <img src={about_photo} alt="About Photo"/>
+            </div>
 
         </div>
 
--- frontend/src/Contact.js
+++ frontend/src/Contact.js
@@ -1,16 +1,20 @@
 import React from "react";
 
+
+
 function Contact() {
 const phoneNumber = '123-456-7891';
 const address = '123 1st Ave New York, NY 00000';
 const email = 'HauteHouseSalon@randomsite.com'
-
+const contact_image = "https://i.ibb.co/Mc7PQPx/single-process-hair-dye.jpg"
     return (
         <div>
-            <h2 className="text-3xl text-gray-700 font-bold mb-5" >Contact Us</h2>
+            
+            <h2 className="text-7xl text-center" >Contact Us</h2>
             <p>Phone: {phoneNumber}</p>
             <p>Address: {address}</p>
             <p>Email: {email}</p>
+            <img src={contact_image} alt="Contact Photo"/>
         </div>
 
     )
