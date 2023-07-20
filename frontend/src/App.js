ceee9d3c00527f0bf62e8cef2427a341a0368598
Bryant Wiggins <topwigit@gmail.com>
2023-07-20 14:07:25 -0400

    added fetch and mapped cards

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,25 @@
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import Home from './Home'
 import Navbar from './Header/Navbar';
 import Banner from './Header/Banner'
-import React from 'react';
 import Stylists from './Stylists';
-import Stylist from './Stylist';
 import About from './About';
 import Contact from './Contact';
-
-
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import './index.css';
 
 function App() {
 
+  const [stylists, setStylists] = useState([])
 
+  useEffect(() => {
+    fetch("/stylists")
+    .then(resp => resp.json())
+    .then(data => setStylists(data))
+  },[])
+
+console.log(stylists)
 
   return (
     <div className='main'>
@@ -25,8 +30,8 @@ function App() {
       <div>
         <Routes>
           <Route path ='/' element={<Home />}></Route>
-          <Route path='/Stylists' element={<Stylists />}></Route>
-          <Route path='/Stylists/:id' element={<Stylist />}></Route>
+          <Route path='/Stylists' element={<Stylists stylists={stylists} />}></Route>
+          {/* <Route path='/Stylists/:id' element={<Stylist />}></Route> */}
           <Route path='/About' element={<About />}></Route>
           <Route path='/Contact' element={<Contact />}></Route>
         </Routes>
--- frontend/src/Stylist.js
+++ frontend/src/Stylist.js
@@ -1,10 +1,20 @@
 import React from "react";
 
-function Stylist() {
-    return (
-        <div>
+function Stylist({name, info, services }) {
 
-        </div>
+
+    return (
+<div className="max-w-sm rounded overflow-hidden shadow-lg bg-antiflashwhite border-green-600 border-2">
+
+  {/* <img className="object-scale-down h-48 w-96" src={image} alt={gameTitle}/> This is where we hardcode an image*/}
+
+  <div className="px-6 py-4">
+    <h1 className="font-bold text-xl mb-2">{name}</h1>
+    <h2 className="font-style: italic font font-semibold text-xs mb-2">{info}</h2>
+    <h3 className ="font-style: italic font font-semibold text-xs mb-2" >{services}</h3>
+  </div>
+
+</div>
     )
 }
 export default Stylist
