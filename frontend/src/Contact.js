f4b591ca7692f4ceb48ba6d7860a8393b6abb0a5
Lillian Oquendo <LillianOquendo1@gmail.com>
2023-07-19 21:55:08 -0400

    updated contact us page

--- frontend/src/Contact.js
+++ frontend/src/Contact.js
@@ -1,11 +1,16 @@
 import React from "react";
 
 function Contact() {
-
+const phoneNumber = '123-456-7891';
+const address = '123 1st Ave New York, NY 00000';
+const email = 'HauteHouseSalon@randomsite.com'
 
     return (
         <div>
-
+            <h2 className="text-3xl text-gray-700 font-bold mb-5" >Contact Us</h2>
+            <p>Phone: {phoneNumber}</p>
+            <p>Address: {address}</p>
+            <p>Email: {email}</p>
         </div>
 
     )
