d40cfe026883f4c9905b5e106a89d5c1ef1f46f5
Lillian Oquendo <LillianOquendo1@gmail.com>
2023-07-21 09:10:34 -0400

    appointments page

--- frontend/src/Stylists.js
+++ frontend/src/Stylists.js
@@ -17,7 +17,7 @@ function Stylists({stylists}) {
             <div className="p-60 pt-12 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4 gap-4">
                 {mappedStylists}
             </div>
-
+            
 
         </div>
 
