e0e10f25dba9f6317ce9aa42de4edd0877fcd50d
Anthony Ramirez <tonyjo.ramirez@gmail.com>
2023-07-21 10:25:17 -0400

    changes

--- frontend/src/Appointment.js
+++ frontend/src/Appointment.js
@@ -3,20 +3,22 @@ import AppointmentCard from './AppointmentCard'
 
 
 function Appointment({newAppointment}){
-const mappedAppt = newAppointment.map((newAppointment) =>(
-    <AppointmentCard
-    name = {newAppointment.client_name}
-    date = {newAppointment.app_date_time}
-    service = {newAppointment.service}
-    />
-))
+    const mappedAppt = newAppointment.map((newAppointment) =>(
+        <AppointmentCard
+        name = {newAppointment.client_name}
+        date = {newAppointment.app_date_time}
+        service = {newAppointment.service}
+        />
+    ))
+
     return (
         <div className="stylist-container">
             <div className="p-60 pt-12 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4 gap-4">
                 {mappedAppt}
             </div>
         </div>
-    )}   
+    )
+}   
 
 export default Appointment
 
